7fb1e07cff121b7816c13e2bacc7fc72bfa47f75
tryfulhalckme <erdenistars@gmail.com>
2025-11-10 02:43:26 +0300

    sad

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ Main application file
 from flask import Flask
 from src.api import auth, students, teachers, disciplines, grades, specialties
 
-app = Flask(__name__)
+# Configure the Flask app to serve static files from the 'src' directory
+app = Flask(__name__, static_folder='src', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return app.send_static_file('index.html')
 
 # Register auth blueprints
-app.add_url_rule('/register', 'register', auth.register, methods=['POST'])
 app.add_url_rule('/login', 'login', auth.login, methods=['POST'])
 
 # Register student blueprints
 app.add_url_rule('/students', 'create_student', students.create_student, methods=['POST'])
 app.add_url_rule('/students', 'get_students', students.get_students, methods=['GET'])
 app.add_url_rule('/students/<int:student_id>/archive', 'archive_student', students.archive_student, methods=['POST'])
+app.add_url_rule('/students/<int:student_id>/grades', 'get_student_grades', grades.get_student_grades, methods=['GET'])
 
 # Register teacher blueprints
 app.add_url_rule('/teachers', 'create_teacher', teachers.create_teacher, methods=['POST'])
@@ -27,7 +32,6 @@ app.add_url_rule('/disciplines', 'get_disciplines', disciplines.get_disciplines,
 
 # Register grade blueprints
 app.add_url_rule('/grades', 'add_grade', grades.add_grade, methods=['POST'])
-app.add_url_rule('/students/<int:student_id>/grades', 'get_student_grades', grades.get_student_grades, methods=['GET'])
 
 # Register specialty blueprints
 app.add_url_rule('/specialties', 'create_specialty', specialties.create_specialty, methods=['POST'])
